7ab6719b1134b9382f0a3f2caea2f3e31e11841f
Yuto Yama <yuvto.0420@gmail.com>
2024-12-01 21:49:52 +0900

    add assert

--- CS50/test/test_calculator.py
+++ CS50/test/test_calculator.py
@@ -1,8 +1,22 @@
 from CS50.function.calculator import square
+import pytest
 
-def test_square():
-    assert square(0) == 0
+def test_positive():
+    assert square(1) != 4
     assert square(1) == 1
     assert square(2) == 4
     assert square(3) == 9
     assert square(10) == 100
+
+def test_negative():
+    assert square(-1) == 1
+    assert square(-2) == 4
+    assert square(-3) == 9
+    assert square(-10) == 100
+
+def test_zero():
+    assert square(0) == 0
+    assert square(0) != 1
+    assert square(0) != 4
+    assert square(0) != 9
+    assert square(0) != 100
